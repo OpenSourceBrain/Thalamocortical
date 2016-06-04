4bbde89a1fb9b21e35890f93217197d4d150fb24
RokasSt <rokas.stanislovas.13@ucl.ac.uk>
2016-06-04 02:53:57 +0100

    deleted files that can be regenerated

--- NeuroML2/Cell1-supppyrRS-FigA1RS/Cell1-supppyrRS-FigA1RS_0.002500/runsim.sh
+++
@@ -1,2 +0,0 @@
-export JNML_HOME=/home/rokas/neuroConstruct/jNeuroMLJar
-/home/rokas/neuroConstruct/jNeuroMLJar/jnml /home/rokas/Thalamocortical/neuroConstruct/pythonScripts/../generatedNeuroML2/LEMS_Thalamocortical.xml
