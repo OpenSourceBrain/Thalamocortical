0af28efaf919b5aa1f9a2d168db0038fffdaba26
Padraig Gleeson <p.gleeson@gmail.com>
2016-08-01 16:07:08 +0100

    Renamed param in spiny stellate cell

--- neuroConstruct/generatedNeuroML2/updateNML2.sh
+++ neuroConstruct/generatedNeuroML2/updateNML2.sh
@@ -1,2 +1,8 @@
+sed -i 's:include href=":include href="../channels/:g' *cell.nml
+
 cp *.cell.nml ../../NeuroML2/cells/
+
+sed -i 's:include href="../channels/:include href=":g' *cell.nml
+
+
 cp *.channel.nml cad*nml ../../NeuroML2/channels
